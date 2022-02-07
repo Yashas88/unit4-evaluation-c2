7a5b1582fb6dff6c194f0a5eacb18e04c15efa48
Yashas D B <yashasbasavaraj@gmail.com>
2022-02-07 17:05:43 +0530

    Update bank.controller.js

--- src/controllers/bank.controller.js
+++ src/controllers/bank.controller.js
@@ -3,6 +3,13 @@ const Bankdb = require("../models/bank.model");
 
 const router = express.Router();
 
-
+router.post("", async (req, res) => {
+    try {
+        const Bankdb = await Bankdb.create(req.body);
+        returnres.status(200).send(Bankdb);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+})
 
 module.exports = router;
