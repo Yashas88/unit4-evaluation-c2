ed66be244489bde2b85295c5e69943e8bbc4dc6d
Yashas D B <yashasbasavaraj@gmail.com>
2022-02-07 15:25:07 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 const express = require("express");
-const bankController = require("./controllers/")
+const bankController = require("./controllers/bank.controller");
+
+const connect = require("./configs/db");
+const app = express();
+
+const PORT = 4000;
+app.use(express.json());
+
+app.use("/bankdatabase", bankController);
+
+app.get("/", (req, res) => {
+     console.log("welcome to bank");
+     res.json("welcome to bank");
+});
+
+app.listen(PORT, async function () {
+    try {
+        await connect();
+        console.log(`Server is running at ${PORT}`);
+    } catch(e) {
+        console.log("error is ", e.message);
+    }
+})
+
+
+// console.log("welcome to bank")
